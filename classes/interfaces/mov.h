542e5304ec2bedab3958902d25ecb0221b9c140b
JVictorGalvao <joaogalvao@eng.ci.ufpb.br>
2022-12-11 11:44:29 -0300

    feat: Implementa movimento de vizinhança

---
+++ classes/interfaces/mov.h
@@ -0,0 +1,19 @@
+#ifndef MOV_H
+#define MOV_H
+
+#include "instance.h"
+#include <vector>
+#include <algorithm>
+#include "sled.h"
+
+class Mov {
+  public:
+    std::vector<Sled> min_sleds;
+    std::vector<Gift> removed_gifts;
+
+    Mov(Instance &instance);
+    Mov();
+    void printSolution();
+};
+
+#endif
